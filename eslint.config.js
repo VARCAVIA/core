d5e91e956832569cb0f75496091516064a697f3d
mrmargarella <mr.margarella@gmail.com>
2025-06-12 22:37:36 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,25 @@
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import reactPlugin from 'eslint-plugin-react'
 
 export default [
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    ignores: ["node_modules", "dist"],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules', 'dist', 'frontend/dist'],
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: "module",
+      sourceType: 'module',
+      parser: '@babel/eslint-parser',
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: { presets: ['@babel/preset-react'] },
+      },
     },
-    plugins: {
-      prettier: eslintPluginPrettier,
-    },
+    plugins: { prettier: eslintPluginPrettier, react: reactPlugin },
     rules: {
-      "prettier/prettier": "error",
+      'prettier/prettier': 'error',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
     },
+    settings: { react: { version: 'detect' } },
   },
-];
+]
